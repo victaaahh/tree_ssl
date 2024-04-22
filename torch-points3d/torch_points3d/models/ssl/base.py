dd0e2c1df067e1e3708eccefa8cb7c7a4e906806
victaaahh <victor.yttesen@outlook.com>
2024-04-22 21:30:46 +0200

    Continued work on model. Defined most of loss func and forward func. Also the set_input for minkowski. And started on encoder and expander

--- torch-points3d/torch_points3d/models/ssl/base.py
+++ torch-points3d/torch_points3d/models/ssl/base.py
@@ -1,6 +1,9 @@
 from torch_points3d.models.base_model import BaseModel
 from torch_points3d.models.model_interface import InstanceTrackerInterface
 
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
 
 class VICRegBase(BaseModel, InstanceTrackerInterface):
     def __init__(self, opt):
@@ -9,23 +12,44 @@ class VICRegBase(BaseModel, InstanceTrackerInterface):
         self.loss_scaling = opt.loss_scaling
 
         self.loss_names = ["vicreg_loss"]
-        self.model_names = []
-        self.visual_names = []
-        self.optimizers = []
         
         self.encoder = None
+        
+        # Define expander in base class?
         self.expander = None
     
     def forward(self, *args, **kwargs):
         assert self.encoder is not None and self.expander is not None
 
-        Y1 = self.encoder(self.input1)
-        Y2 = self.encoder(self.input2)
+        Z1 = self.expander(self.encoder(self.input1))
+        Z2 = self.expander(self.encoder(self.input2))
         
-        Z1 = self.expander(Y1)
-        Z2 = self.expander(Y2)
-        
-        self.loss = self.vicreg_loss(Z1, Z2, self.loss_scaling)
+        # Should we split loss into multiple losses and use the inbuilt scaling functionality?
+        # Then perhaps we can monitor them individually
+        self.vicreg_loss = self.compute_vicreg_loss(Z1, Z2, self.loss_scaling)
     
-    def vicreg_loss(self, Z1, Z2, scaling):
-        pass
+    def compute_vicreg_loss(self, Z1, Z2, scaling, eps=0.0001, gamma=1):
+        # How should I get information on dimension D and batch size N? From config or from measuring?
+        
+        # Batch size and vec size:
+        N, D = Z1.shape
+
+        inv_loss = F.mse_loss(Z1, Z2)
+        
+        std_Z1 = torch.sqrt(torch.var(Z1, dim=0) + eps)
+        std_Z2 = torch.sqrt(torch.var(Z2, dim=0) + eps)
+        var_loss = torch.mean(F.relu(gamma - std_Z1)) / 2 + torch.mean(F.relu(gamma - std_Z2)) / 2
+        
+        #Z1 = Z1 - torch.mean(Z1, dim=0)
+        #Z2 = Z2 - torch.mean(Z2, dim=0)
+        
+        cov_Z1 = (Z1.T @ Z1) / (N - 1)
+        cov_Z2 = (Z2.T @ Z2) / (N - 1)
+        cov_Z1_no_diag = cov_Z1[~torch.eye(D)]
+        cov_Z2_no_diag = cov_Z2[~torch.eye(D)]
+
+        cov_loss = cov_Z1_no_diag.pow(2).sum() / D + \
+                   cov_Z2_no_diag.pow(2).sum() / D
+
+        loss = scaling["invariance"] * inv_loss + scaling["variance"] * var_loss + scaling["covariance"] * cov_loss
+        return loss
--- torch-points3d/torch_points3d/models/ssl/minkowski.py
+++ torch-points3d/torch_points3d/models/ssl/minkowski.py
@@ -1,15 +1,32 @@
 from torch_points3d.models.ssl.base import VICRegBase
 from torch_points3d.modules.MinkowskiEngine import initialize_minkowski_unet
 
+import torch
 import torch.nn as nn
+import MinkowskiEngine as ME
+from torch_geometric.data import Batch
 
 class MinkowskiVICReg(VICRegBase):
     def __init__(self, opt, dataset):
         super().__init__(opt)
         self.encoder = initialize_minkowski_unet(opt.encoder_name, dataset.feature_dimension, opt.representation_D, D=opt.D)
-        self.expander = nn.Sequential()
+        
+        # Add non-linearity and batch normalization
+        self.expander = nn.Sequential(nn.Linear(opt.representation_D, opt.expander_dims[0]),
+                                      *[nn.Linear(opt.expander_dims[i], opt.expander_dims[i+1]) for i in range(len(opt.expander_dims)-1)])
 
-    def set_input(self, input, device):
+    def set_input(self, data, device):
         # unpack data from dataset and apply preprocessing
-        self.input = ...
-    
+
+        # self.batch_idx = data.batch.squeeze()
+        
+        data = data.to_data_list()
+        # Maybe theres a faster/better way than slicing
+        X1 = Batch.from_data_list(data[::2])
+        X2 = Batch.from_data_list(data[1::2])
+
+        coords1 = torch.cat([X1.batch.unsqueeze(-1).int(), X1.coords.int()], -1)
+        coords2 = torch.cat([X2.batch.unsqueeze(-1).int(), X2.coords.int()], -1)
+
+        self.input1 = ME.SparseTensor(features=X1.x, coordinates=coords1, device=device)
+        self.input2 = ME.SparseTensor(features=X2.x, coordinates=coords2, device=device)
