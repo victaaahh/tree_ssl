3ca39fe56f674a34eda3f17455482f4922e93299
victaaahh <79800808+victaaahh@users.noreply.github.com>
2024-04-29 00:09:51 +0200

    Changes from dev branch (#2)
    Summary  of changes:
     - Implemented base and minkowski model.
    
     - Implemented double batching.
    
     - Made training config.
    
     - Some additional minor additions and name changes.
    
    
    All changes:
    
    * More inital work on model.
    
    * Overloaded num_classes property and made it return 0. This avoids looping over the entire dataset when instantiating model
    
    * Basic structure of base and minkowski model defined.
    
    * Implemented double batching
    such that each point cloud appears twice in a row within a batch with different transforms applied to each
    
    * Changed to use base_tracker, which might work and be enough
    
    * Changed dataset filename to fit with naming scheme
    
    * Continued work on model. Defined most of loss func and forward func. Also the set_input for minkowski. And started on encoder and expander
    
    * Small changes to model definition:
    Minor change to VICRegBase class.
    Added things to and improved ssl/minkowski.yaml config.
    Continued work on encoder and expander in minkowski vicreg.
    
    * Adding training config for minkowski vicreg
    
    * Fixed error in loss function and moved foward functin to minkowski class to allow for making the representation vector dense in an easy way.
    
    * Added tracking of individual loss components.
    
    * Added comment as a reminder to only use even batch sizes
    
    * raise an error in case of uneven batch sizes
    
    * Three changes:
    Changed final layer of minkowski vicreg to identity function.
    
    Uncommented self.batch_idx as it is part of the tracker interface. I also use self.input ant self.output as they are also part of the tracker interface. It could prove useful later on.
    
    Updated model conf to reflect changes and cleaned up training conf and added more config loggings to wandb.
    
    * Updated environment files to include a version of mkl that works with pytorch. See https://github.com/pytorch/pytorch/issues/123097
    
    * Fixed mistake in minkowski model expander and a few mistakes in training config

--- torch-points3d/torch_points3d/models/ssl/base.py
+++ torch-points3d/torch_points3d/models/ssl/base.py
@@ -1,7 +1,41 @@
 from torch_points3d.models.base_model import BaseModel
 from torch_points3d.models.model_interface import InstanceTrackerInterface
 
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
 
-class SSLBase(BaseModel, InstanceTrackerInterface):
-    def __init__(self):
-        pass
+class VICRegBase(BaseModel):
+    def __init__(self, opt, model_type, dataset, modules):
+        super().__init__(opt)
+        
+        self.loss_scaling = opt.loss_scaling
+        self.loss_eps = opt.loss_eps
+        self.loss_gamma = opt.loss_gamma
+
+        # Not quite sure what this does:
+        self.loss_names = ["vicreg_loss", "var_loss", "inv_loss", "cov_loss"]
+        
+    def compute_vicreg_loss(self, Z1, Z2, scaling, eps=0.0001, gamma=1):
+        # Batch size and vec size:
+        N, D = Z1.shape
+
+        inv_loss = F.mse_loss(Z1, Z2)
+        
+        std_Z1 = torch.sqrt(torch.var(Z1, dim=0) + eps)
+        std_Z2 = torch.sqrt(torch.var(Z2, dim=0) + eps)
+        var_loss = torch.mean(F.relu(gamma - std_Z1)) + torch.mean(F.relu(gamma - std_Z2))
+        
+        Z1 = Z1 - torch.mean(Z1, dim=0)
+        Z2 = Z2 - torch.mean(Z2, dim=0)
+        
+        cov_Z1 = (Z1.T @ Z1) / (N - 1)
+        cov_Z2 = (Z2.T @ Z2) / (N - 1)
+        cov_Z1_no_diag = cov_Z1[~torch.eye(D, dtype=bool)]
+        cov_Z2_no_diag = cov_Z2[~torch.eye(D, dtype=bool)]
+
+        cov_loss = cov_Z1_no_diag.pow(2).sum() / D + \
+                   cov_Z2_no_diag.pow(2).sum() / D
+
+        loss = scaling["invariance"] * inv_loss + scaling["variance"] * var_loss + scaling["covariance"] * cov_loss
+        return loss, var_loss, inv_loss, cov_loss
---
+++ torch-points3d/torch_points3d/models/ssl/minkowski.py
@@ -0,0 +1,61 @@
+from torch_points3d.models.ssl.base import VICRegBase
+from torch_points3d.modules.MinkowskiEngine import initialize_minkowski_unet
+
+import torch
+import torch.nn as nn
+import MinkowskiEngine as ME
+from torch_geometric.data import Batch
+
+class MinkowskiVICReg(VICRegBase):
+    def __init__(self, opt, model_type, dataset, modules):
+        super().__init__(opt, model_type, dataset, modules)
+        self.encoder = initialize_minkowski_unet(model_name=opt.encoder,
+                                                 in_channels=dataset.feature_dimension,
+                                                 # For SENet out_channels is only used in the final layer which is overwritten below
+                                                 out_channels=opt.get("representation_D", 512),
+                                                 D=opt.D,
+                                                 **opt.encoder_options)
+        
+        self.encoder.final = nn.Identity()
+        
+        if opt.expander_activation != "relu":
+            raise NotImplementedError("Only 'relu' is supported right now")
+
+        layers = []
+        for i in range(len(opt.expander_layers)-1):
+            layers.append(nn.ReLU())
+            layers.append(nn.BatchNorm1d(opt.expander_layers[i]))
+            layers.append(nn.Linear(opt.expander_layers[i], opt.expander_layers[i+1]))
+
+        self.expander = nn.Sequential(nn.Linear(self.encoder.PLANES[-1], opt.expander_layers[0]), *layers)
+
+    def set_input(self, data, device):
+        # unpack data from dataset and apply preprocessing
+
+        self.batch_idx = data.batch.squeeze()
+        
+        data = data.to_data_list()
+
+        X1 = Batch.from_data_list(data[::2])
+        X2 = Batch.from_data_list(data[1::2])
+
+        coords1 = torch.cat([X1.batch.unsqueeze(-1).int(), X1.coords.int()], -1)
+        coords2 = torch.cat([X2.batch.unsqueeze(-1).int(), X2.coords.int()], -1)
+
+        input1 = ME.SparseTensor(features=X1.x, coordinates=coords1, device=device)
+        input2 = ME.SparseTensor(features=X2.x, coordinates=coords2, device=device)
+        self.input = (input1, input2)
+
+    def forward(self, *args, **kwargs):
+        input1, input2 = self.input
+
+        Y1 = self.encoder(input1)
+        Y2 = self.encoder(input2)
+        
+        self.output = (Y1, Y2)
+        
+        Z1 = self.expander(Y1.F)
+        Z2 = self.expander(Y2.F)
+
+        self.vicreg_loss, self.var_loss, self.inv_loss, self.cov_loss = self.compute_vicreg_loss(Z1, Z2, self.loss_scaling, self.loss_eps, self.loss_gamma)
+        self.loss = self.vicreg_loss
