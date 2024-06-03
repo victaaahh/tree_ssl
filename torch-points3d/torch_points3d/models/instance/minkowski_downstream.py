bcf8b508c50f8694f2e0cb0f97e65f9b6a233759
victaaahh <victor.yttesen@outlook.com>
2024-06-03 10:56:40 +0200

    Added support for using weights from a previous downstream run. Added options to config accordingly.
    Also added 0.01 drop_path.

--- torch-points3d/torch_points3d/models/instance/minkowski_downstream.py
+++ torch-points3d/torch_points3d/models/instance/minkowski_downstream.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 import torch.nn as nn
 
@@ -6,9 +8,17 @@ import MinkowskiEngine as ME
 from torch_points3d.modules.MinkowskiEngine import initialize_minkowski_unet
 from torch_points3d.models.instance.minkowski import MinkowskiBaselineModel, SeparateLinear
 
+log = logging.getLogger(__name__)
+
+
 class MinkowskiDownstream(MinkowskiBaselineModel):
     def __init__(self, option, model_type, dataset, modules):
         super(MinkowskiBaselineModel, self).__init__(option, model_type, dataset, modules)
+
+        load_ssl_model = bool(option.get("ssl_model_path", None))
+        load_downstream_model = bool(option.get("downstream_model_path", None))
+        assert load_ssl_model != load_downstream_model, "Only specify ssl_model_path or downstream_model_path"
+
         self.model = initialize_minkowski_unet(model_name=option.model_name,
                                                  in_channels=dataset.feature_dimension,
                                                  out_channels=dataset.num_classes,
@@ -16,11 +26,13 @@ class MinkowskiDownstream(MinkowskiBaselineModel):
                                                  **option.kwargs)
         
         in_channel = self.model.final.linear.weight.shape[1]
-        self.model.final = nn.Identity()
 
-        state_dict = torch.load(option.ssl_model_path)["models"][option.ssl_weight_name]
-        state_dict = {key[8:]: val for key, val in state_dict.items() if key.startswith("encoder")}
-        self.model.load_state_dict(state_dict)
+        if load_ssl_model:
+            log.info("Will use encoder weights from self-supervised model")
+            self.model.final = nn.Identity()
+            state_dict = torch.load(option.ssl_model_path)["models"][option.ssl_weight_name]
+            state_dict = {key[8:]: val for key, val in state_dict.items() if key.startswith("encoder")}
+            self.model.load_state_dict(state_dict)
         
         if option.kwargs.get("dropout", 0) > 0:
             self.model.glob_avg = nn.Sequential(
@@ -38,6 +50,12 @@ class MinkowskiDownstream(MinkowskiBaselineModel):
             if m.bias is not None:
                 torch.nn.init.constant_(m.bias, 0)
 
+        if load_downstream_model:
+            log.info("Will use weight from previous downstream_model")
+            state_dict = torch.load(option.downstream_model_path)["models"][option.downstream_weight_name]
+            state_dict = {key[6:]: val for key, val in state_dict.items() if key.startswith("model")}
+            self.model.load_state_dict(state_dict)
+
         self.head_namespace = option.get("head_namespace", "final.linears")
         self.head_optim_settings = option.get("head_optim_settings", {})
         self.backbone_optim_settings = option.get("backbone_optim_settings", {})
@@ -46,12 +64,14 @@ class MinkowskiDownstream(MinkowskiBaselineModel):
         
         self.mode = option.mode
         if self.mode == "freeze":
+            log.info("Training with frozen encoder")
             self.model.requires_grad_(False)
             self.model.eval()
             self.model.final.requires_grad_(True)
             self.model.final.train()
             self.enable_dropout_in_eval()
         elif self.mode == "finetune":
+            log.info("Training without frozen encoder")
             pass
         else:
-            raise ValueError("Only 'freeze' and 'finetune' mode is supported")
+            raise ValueError("Only 'freeze' and 'finetune' modes are supported")
